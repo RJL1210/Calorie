2c16d2c1a081bd0dcbf3c4eb75a48718943aba19
RJL1210 <144273649+RJL1210@users.noreply.github.com>
2024-02-08 23:32:21 -0800

    first commit, added nutrition api

--- counter/views.py
+++ counter/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
+
 def home(request):
-    
+
+
+    query = '1lb brisket and fries'
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    response = requests.get(api_url, headers={'X-Api-Key': 'none'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+
     return render(request, 'home.html')
